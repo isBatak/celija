fea248b6417e1a45278ba8bb44d4e4091764c39d
Ivica Batinić <ivicabatinic123@gmail.com>
2023-11-05 14:21:25 +0100

    update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,10 @@ export default function Home() {
 		<Container py={{ base: '12', md: '16' }} maxW="3xl">
 			<Logo />
 
+			<Text mb="10" fontSize="xl" textAlign="center">
+				Iris Poljan & Josip Kresović
+			</Text>
+
 			<Text mt="10" textAlign="justify">
 				U svijetu gdje su granice između umjetnosti i administracije zamagljene, gdje birokracija
 				često zasjenjuje kreativnost, "ĆELIJA" propitkuje bit kulture, ulogu umjetnosti u našim
@@ -20,32 +24,27 @@ export default function Home() {
 				uvijek naših predrasuda. Ima moć šokiranja, često nas ostavi zbunjenima, a u nekim
 				slučajevima, propitujući sama svoju bit. "ĆELIJA" nas podsjećajuća da je umjetnost ipak
 				subjektivno područje, otvoreno za tumačenje svima. S tim na umu, umjetnicima uvijek treba
-				pomoć, a kulturu gradimo skupa. Razmisli što je tebi bitno za kulturu i umjetnost;
-				metaforički ili konkretno, ostavljamo ti da sam procijeniš, i niže ispuni svoju ĆELIJU
-				jednom riječi. Npr. Sloboda, financiranje, kreativnost, protokol… Svoj doprinos pronađi u
-				Galeriji Matice hrvatske na otvorenju ĆELIJE 14.11. u 19h.
+				pomoć, a kulturu gradimo skupa.
 			</Text>
 
-			<Heading mt="10" fontSize="xl">
-				Pridruite nam se u live streamu....
-			</Heading>
-			<Text textAlign="justify">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna justo, mattis vitae
-				posuere quis, hendrerit ut turpis. Praesent eget facilisis eros.
+			<Text mt="10" textAlign="justify">
+				Razmisli što je tebi bitno za kulturu i umjetnost; metaforički ili konkretno, ostavljamo ti
+				da sam procijeniš, i niže ispuni svoju ĆELIJU jednom riječi. Npr. Sloboda, financiranje,
+				kreativnost, protokol…
 			</Text>
 
-			<Button>Link na Google Sheet</Button>
-
-			<Heading mt="10" fontSize="xl">
-				Pridruzite nam se
-			</Heading>
-			<Text textAlign="justify">
-				Ako zelite doprinjeti nasem projektu javite se porukom ovdje...
+			<Text mt="10" mb="2" textAlign="justify">
+				Svoj doprinos pronađi u Galeriji Matice hrvatske na otvorenju ĆELIJE 14.11. u 19h.
 			</Text>
 
 			<ContributionForm>
-				<Button>Otvori Obrazac</Button>
+				<Button>Otvori obrazac</Button>
 			</ContributionForm>
+
+			<Text mt="10" mb="2" textAlign="justify">
+				Performans uživo pratite na ovom linku:
+			</Text>
+			<Button>Link na live performans</Button>
 		</Container>
 	);
 }
