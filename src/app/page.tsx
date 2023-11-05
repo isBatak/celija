014139db1700cec47e44a0d262090afd70dabe5e
Ivica Batinić <ivicabatinic123@gmail.com>
2023-11-05 14:24:19 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,14 +17,14 @@ export default function Home() {
 
 			<Text mt="10" textAlign="justify">
 				U svijetu gdje su granice između umjetnosti i administracije zamagljene, gdje birokracija
-				često zasjenjuje kreativnost, "ĆELIJA" propitkuje bit kulture, ulogu umjetnosti u našim
-				životima i dihotomije koje definiraju opće razumijevanje umjetničkog izražavanja. Je li
-				"ĆELIJA" kritika administrativnih zavrzlama koje određuju sudbinu kulture ili je oda
-				otpornosti mladih umjetnika koji se usuđuju prkositi statusu quo? Umjetnost se ne pridržava
-				uvijek naših predrasuda. Ima moć šokiranja, često nas ostavi zbunjenima, a u nekim
-				slučajevima, propitujući sama svoju bit. "ĆELIJA" nas podsjećajuća da je umjetnost ipak
-				subjektivno područje, otvoreno za tumačenje svima. S tim na umu, umjetnicima uvijek treba
-				pomoć, a kulturu gradimo skupa.
+				često zasjenjuje kreativnost, &quot;ĆELIJA&quot; propitkuje bit kulture, ulogu umjetnosti u
+				našim životima i dihotomije koje definiraju opće razumijevanje umjetničkog izražavanja. Je
+				li &quot;ĆELIJA&quot; kritika administrativnih zavrzlama koje određuju sudbinu kulture ili
+				je oda otpornosti mladih umjetnika koji se usuđuju prkositi statusu quo? Umjetnost se ne
+				pridržava uvijek naših predrasuda. Ima moć šokiranja, često nas ostavi zbunjenima, a u nekim
+				slučajevima, propitujući sama svoju bit. &quot;ĆELIJA&quot; nas podsjećajuća da je umjetnost
+				ipak subjektivno područje, otvoreno za tumačenje svima. S tim na umu, umjetnicima uvijek
+				treba pomoć, a kulturu gradimo skupa.
 			</Text>
 
 			<Text mt="10" textAlign="justify">
